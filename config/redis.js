446aecbbfa2421c83b4629c44a1eb70e573ac8fd
dhruv gupta <dhruv4516askn@gmail.com>
2026-04-09 12:16:31 +0530

    Redis Cloud configured successfully

--- config/redis.js
+++ config/redis.js
@@ -3,9 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379'
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: process.env.REDIS_URL,
+        port: process.env.REDIS_PORT
+    }
 });
+// const redisClient = redis.createClient({
+//   url: process.env.REDIS_URL || 'redis://localhost:6379'
+// });
 
 redisClient.on('error', (err) => {
   console.error('Redis error:', err);
@@ -22,5 +31,17 @@ const connectRedis = async () => {
     console.error('Redis connection error:', error);
   }
 };
+
+// import { createClient } from 'redis';
+
+// client.on('error', err => console.log('Redis Client Error', err));
+
+// await client.connect();
+
+// await client.set('foo', 'bar');
+// const result = await client.get('foo');
+// console.log(result)  // >>> bar
+
+
 
 export { redisClient, connectRedis };
